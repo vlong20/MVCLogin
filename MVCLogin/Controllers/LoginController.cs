3500cee45072a9f1a9ef470d42da054e005b2969
vlong20 <124040142+vlong20@users.noreply.github.com>
2023-03-24 10:33:38 +0700

    Add project files.

---
+++ MVCLogin/Controllers/LoginController.cs
@@ -0,0 +1,39 @@
+﻿using MVCLogin.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCLogin.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Autherize(MVCLogin.Models.Account account)
+        {
+            using (AccountDataEntities db = new AccountDataEntities())
+            {
+                var userDetails = db.Accounts.Where(x => x.username == account.username && x.password == account.password).FirstOrDefault();
+                if (userDetails == null)
+                {
+                    account.LoginErrorMessage = "Wrong username or password";
+                    return View("Index", account);
+                }
+                else
+                {
+                    Session["MaNV"] = userDetails.MaNV;
+                    if (userDetails.permission == 0)
+                        return RedirectToAction("Index", "Admin");
+                    else
+                        return RedirectToAction("Index", "Staff");
+                }
+            }
+        }
+    }
+}
---
+++ MVCLogin/assets/js/addbook.js
@@ -0,0 +1,16 @@
+function wrapper_show(){
+    document.getElementById("wrapper").style.display = "block";
+    document.getElementById("header__menu").style.display = "none";
+}
+
+function back_to_index(){
+    window.location.href = "./index.html";
+}
+
+function show(){
+    document.getElementById("book__add__submit__pop-up").style.display = "block";
+}
+
+/*
+Thêm dữ liệu sách vào csdl ở đây nhé
+*/
